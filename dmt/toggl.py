3c7ecc89350e562f4fe4763c78215a8c1f0e8b35
kedod <kedodek@protonmail.com>
2018-03-12 22:48:50 +0100

    add tag_time_entries method

--- dmt/toggl.py
+++ dmt/toggl.py
@@ -8,6 +8,9 @@ class Toggl(object):
     def get_time_entries(self, start_time='', end_time=''):
         return self.toggl.get_time_entries(start_time=start_time, end_time=end_time)
 
+    def tag_time_entries(self, time_entries):
+        return self.toggl.tag_time_entries(time_entries)
+
 
 class BaseToggl(object):
     def setup_toggl(self, api_url, token):
@@ -17,8 +20,14 @@ class BaseToggl(object):
         return self
 
     def get_time_entries(self, start_time='', end_time=''):
-        url = self._build_url(start_date=start_time, end_date=end_time, category='time_entries')
+        start_date = self._format_date(start_time)
+        end_date = self._format_date(end_time)
+        url = self._build_url(start_date=start_date, end_date=end_date, category='time_entries', separate_sign='?')
         return self.session.get(url).json()
+
+    def tag_time_entries(self, time_entries):
+        for time_entry in time_entries:
+            self._tag_time_entry(time_entry['id'])
 
     def _set_session(self):
         session = requests.Session()
@@ -27,10 +36,21 @@ class BaseToggl(object):
 
     def _build_url(self, **kwargs):
         category = kwargs.pop('category')
-        parameters = '&'.join(['{}={}'.format(kwarg, self._format_date(kwargs[kwarg])) for kwarg in kwargs])
-        url = '{base_url}{cat}?{params}'.format(base_url=self.api_url, cat=category, params=parameters)
+        separate_sign = kwargs.pop('separate_sign')
+        parameters = self._get_parameters(separate_sign, **kwargs)
+        url = '{base_url}{cat}{params}'.format(base_url=self.api_url, cat=category, params=parameters)
         return url
 
     def _format_date(self, date):
         formated_date = date.replace(':', '%3A').replace('+', '%2B').replace('-', '%2D')
         return formated_date
+
+    def _get_parameters(self, separate_sign, **kwargs):
+        if separate_sign == '/':
+            return '/{id}'.format(id=kwargs.pop('id', ''))
+        elif separate_sign == '?':
+            return '?' + '&'.join(['{key}={value}'.format(key=kwarg, value=kwargs[kwarg]) for kwarg in kwargs])
+
+    def _tag_time_entry(self, id):
+        url = self._build_url(id=id, category='time_entries', separate_sign='/')
+        return requests.post('{url}'.format(url=url))
--- tests/test_toggle.py
+++ tests/test_toggle.py
@@ -1,7 +1,7 @@
 import unittest
 import requests_mock
 from dmt.toggl import Toggl
-from tests.fixtures.responses import response
+from tests.fixtures.responses import fixed_time_entries
 
 
 class TestBaseToggle(unittest.TestCase):
@@ -11,8 +11,13 @@ class TestBaseToggle(unittest.TestCase):
 
 @requests_mock.Mocker()
 class TestToggleConnection(TestBaseToggle):
-    def test_response(self, response_mock):
+    def test_get_time_entries_response(self, response_mock):
         response_mock.register_uri('GET', 'https://www.toggl.com/api/v8/time_entries?start_date=&end_date=',
-                                   json=response, status_code=200)
+                                   json=fixed_time_entries, status_code=200)
         time_entries = self.toggle.get_time_entries()
-        self.assertEqual(response, time_entries)
+        self.assertEqual(fixed_time_entries, time_entries)
+
+    def test_tag_time_entries_response(self, response_mock):
+        response_mock.register_uri('POST', 'https://www.toggl.com/api/v8/time_entries/814800050',
+                                   status_code=200)
+        self.toggle.tag_time_entries(fixed_time_entries)
