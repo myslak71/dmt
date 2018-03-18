7b16c937228e1adf631d09a2faf174cb6897413d
kedod <kedodek@protonmail.com>
2018-03-18 07:29:46 +0100

    add abs_path to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
+import os
+
 from setuptools import setup, find_packages
 
-with open('README.md') as file:
+DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(DIR_PATH, 'README.md')) as file:
     long_description = file.read()
 
-install_requires = [line.rstrip('\n') for line in open('requirements.txt')]
+install_requires = [line.rstrip('\n') for line in open(os.path.join(DIR_PATH, 'requirements.txt'))]
 
 setup(
     name='dmt',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     author='kedod',
     author_email='kedod@protonmail.com',
