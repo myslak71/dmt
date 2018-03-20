1481415cdc53431cfc5230487505f4ee6b5187ea
kedod <35638715+kedod@users.noreply.github.com>
2018-03-20 13:52:17 +0100

    Update config.py
    add MEIPASS for pyinstaller

--- dmt/config/config.py
+++ dmt/config/config.py
@@ -1,10 +1,14 @@
 import logging
 import os
+import sys
 from logging import config
 
 import yaml
 
-DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+if getattr(sys, 'frozen', False):
+    DIR_PATH = sys._MEIPASS
+else:
+    DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 
 config.dictConfig(yaml.load(open(os.path.join(DIR_PATH, 'LOGGER.yaml'))))
 LOGGER = logging.getLogger('dmt_logger')
